e9f0688816306516596bff8a97bb682160e9dac5
iva <ivallen84@gmail.com>
2026-08-03 14:26:30 -0600

    Controlador metodos

--- src/ContoladorCalculadora/CalculadoraControl.java
+++ src/ContoladorCalculadora/CalculadoraControl.java
@@ -3,11 +3,126 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ContoladorCalculadora;
-
+ import ModeloCalculadora.CalculadoraModelo;
 /**
  *
  * @author PC
  */
 public class CalculadoraControl {
+
+   private CalculadoraModelo modelo;
+
+    public CalculadoraControl(CalculadoraModelo modelo) {
+        this.modelo = modelo;
+    }
+   
+   
+    
+    
+    
+    
+   public void sumaC(double valor) {
+
+    modelo.setValor(valor);
+    modelo.sumar();
+}
+   public void restaC(double valor) {
+
+    modelo.setValor(valor);
+    modelo.restar();
+}
+    public void multiplicarC(double valor) {
+
+    modelo.setValor(valor);
+    modelo.multiplicar();
+}
+    
+    public void dividirC(double valor){
+        modelo.setValor(valor);
+        modelo.divir();
+    }
+    public String borrarC(String numero) {
+
+    return modelo.borrar(numero);
+}
+   public void reiniciarC() {
+
+    modelo.reiniciar();
+} 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
