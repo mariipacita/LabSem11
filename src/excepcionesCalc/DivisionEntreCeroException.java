03b9b63352a2c323655d162d7406ceea1e898f9a
maripacita <mariichavarriajerez29@gmail.com>
2026-08-03 15:24:13 -0600

    Paquete Excepciones
    Se crea un paquete para las excepciones y se programan las clases

---
+++ src/excepcionesCalc/DivisionEntreCeroException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package excepcionesCalc;
+
+/**
+ *
+ * @author marii
+ */
+public class DivisionEntreCeroException extends Exception {
+    public DivisionEntreCeroException(String mensaje) {
+        super(mensaje);
+    }
+}
